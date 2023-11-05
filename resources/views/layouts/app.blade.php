6548e8eed19dde69f3d7e403dcc2c63581c81264
Alquinn John <alquinngayatao@gmail.com>
2023-11-06 01:08:41 +0800

    append udpate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,11 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  @vite('resources/css/app.css')
-</head>
-<body>
-</body>
-</html>
+    <!doctype html>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    @vite('resources/css/app.css')
+    </head>
+    <body>
+    @yield('content')
+    </body>
+    </html>
+    @include('footer')
