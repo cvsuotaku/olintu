547854e225354aa4a343015ba02f701518b04a1b
Alquinn John Gayatao <alquinngayatao@gmail.com>
2023-12-08 09:57:58 +0800

    centered table

--- resources/views/records.blade.php
+++ resources/views/records.blade.php
@@ -9,12 +9,12 @@
     </div>
 </nav>
 <section class="mb-8">
-    <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-wrap w-full mb-10 flex-col items-center text-center">
+    <div class="container px-5 py-24 mx-auto  items-center text-center">
+        <div class="flex flex-wrap w-full mb-10 flex-col">
             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Record:</h1>
         </div>
-        <div class="relative overflow-x-auto">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <div class="relative overflow-x-auto mb-10">
+            <table class="w-full text-sm text-left text-center rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-6 py-3">
@@ -50,9 +50,9 @@
                 </tbody>
             </table>
         </div>
-        <button class="text-black px-2 py-1 my-2 mx-auto block rounded shadow" style="margin-left: 1rem; background-color: #C8E6C9 ">
+        <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-green-700 hover:bg-green-800 dark:focus:ring-primary-800">
             <a href="{{route('student.dashboard')}}">
-                back
+                Back
             </a>
         </button>
     </div>
