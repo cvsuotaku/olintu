21486923b9f4097420beb4b01af53dda52257684
geiadelarosa <delarosageia0519@gmail.com>
2023-12-08 10:01:08 +0800

    nakalimutan ata

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,7 @@
     <nav class="p-4 text-white" style="background-color: #2E6D32;">
         <div class="container mx-auto">
             <h1 class="text-2xl font-semibold">OLINTU Dashboard</h1>
+            
         </div>
     </nav>
 
