e3b4a0b2b1e0b09e3bad475f4abbb121127084d5
geiadelarosa <delarosageia0519@gmail.com>
2023-12-09 13:22:22 +0800

    profile position fixed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,40 +4,37 @@
 <section class="bg-gray-100 mx-auto">
 
     <!-- Navigation -->
-    <nav class="p-4 text-white" style="background-color: #2E6D32;">
-        <div class="container mx-auto">
-            <h1 class="text-2xl font-semibold">OLINTU Dashboard</h1>
-            <div id="user-profile" class="flex items-center">
-                <div class="flex items-center ms-3">
-                    <div>
-                        <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                            <span class="sr-only">Open user menu</span>
-                            <img class="w-8 h-8 rounded-full" src="{{ asset('images/user-icon.png') }}" alt="user photo">
-                        </button>
-                    </div>
-                    <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
-                        <div class="px-4 py-3" role="none">
-                            <p id="user-profile-name" class="text-sm text-gray-900 dark:text-white" role="none">
-                            </p>
-                            <p id="user-profile-number" class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                            </p>
-                        </div>
-                        <ul class="py-1" role="none">
-                            <li>
-                                <a href="{{ route('grade.records') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Records</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('settings') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
-                            </li>
-                        </ul>
-                    </div>
+    <nav class="p-4 text-white" style="background-color: #2E6D32; display: flex; justify-content: space-between; align-items: flex-start;">
+        <div class="flex items-start">
+            <h1 class="text-2xl font-semibold mt-1">OLINTU Dashboard</h1>
+        </div>
+    
+        <div id="user-profile" class="flex items-start">
+            <button type="button" class="flex text-lg bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                <span class="sr-only">Open user menu</span>
+                <img class="w-8 h-8 rounded-full" src="{{ asset('images/user-icon.png') }}" alt="user photo">
+            </button>
+    
+            <div class="z-50 hidden ml-2 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
+                <div class="px-4 py-3" role="none">
+                    <p id="user-profile-name" class="text-sm text-gray-900 dark:text-white" role="none"></p>
+                    <p id="user-profile-number" class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none"></p>
                 </div>
+                <ul class="py-1" role="none">
+                    <li>
+                        <a href="{{ route('grade.records') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Records</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('settings') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
+    
 
     <!-- Main Content -->
     <div class="container mx-auto mt-8 px-6 py-8 mx-auto lg:py-0">
